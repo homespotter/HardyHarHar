b7e9d90e65e85f3385b44c37f86a31563c2706cb
Peter Keefe <16809252+percula@users.noreply.github.com>
2021-04-09 11:38:07 -0400

    Remove sample android app

---
+++ hardyharhar/build.gradle.kts
@@ -0,0 +1,15 @@
+plugins {
+    `java-library`
+    kotlin("jvm") version "1.4.30"
+    kotlin("plugin.serialization") version "1.4.30"
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.1.0")
+}
